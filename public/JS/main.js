4c8e51b078ad3ca845cf13dabff626a51b4575c4
diaabadr <diaabadr82@gmail.com>
2022-05-27 15:29:13 +0200

    logo

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,34 +1,33 @@
-let nurse = document.querySelector(".home-body .img .image")
-let homeCard = document.querySelector(".home-body .card")
-let loginPage = document.querySelector(".login-page")
-let visitorPage = document.querySelector(".visitor-page")
-let showVisitor = document.querySelector(".show-visitor")
-let showLogin = document.querySelector(".show-login")
+let nurse = document.querySelector(".home-body .img .image");
+let homeCard = document.querySelector(".home-body .card");
+let loginPage = document.querySelector(".login-page");
+let visitorPage = document.querySelector(".visitor-page");
+let showVisitor = document.querySelector(".show-visitor");
+let showLogin = document.querySelector(".show-login");
 
 document.addEventListener("click", function (e) {
-    if (e.target.classList.contains("home-btn")) {
-        window.open("../index.html", "_self")
-        console.log("Gooooooo")
-    }
-    if (e.target.classList.contains("back")) {
-        nurse.classList.remove("image-transform")
-        homeCard.classList.remove("card-transform")
-        loginPage.classList.remove("login-page-show")
-        visitorPage.classList.remove("visitor-page-show")
-        showVisitor.style.display = "none"
-        showLogin.style.display = "none"
-    }
-    if (e.target.classList.contains("login")) {
-        nurse.classList.add("image-transform")
-        homeCard.classList.add("card-transform")
-        loginPage.classList.add("login-page-show")
-        showLogin.style.display = "block"
-    }
-    if (e.target.classList.contains("visitor")) {
-        nurse.classList.add("image-transform")
-        homeCard.classList.add("card-transform")
-        visitorPage.classList.add("visitor-page-show")
-        showVisitor.style.display = "block"
-        
-    }
-})
+  if (e.target.classList.contains("home-btn")) {
+    location.assign("/");
+    console.log("Gooooooo");
+  }
+  if (e.target.classList.contains("back")) {
+    nurse.classList.remove("image-transform");
+    homeCard.classList.remove("card-transform");
+    loginPage.classList.remove("login-page-show");
+    visitorPage.classList.remove("visitor-page-show");
+    showVisitor.style.display = "none";
+    showLogin.style.display = "none";
+  }
+  if (e.target.classList.contains("login")) {
+    nurse.classList.add("image-transform");
+    homeCard.classList.add("card-transform");
+    loginPage.classList.add("login-page-show");
+    showLogin.style.display = "block";
+  }
+  if (e.target.classList.contains("visitor")) {
+    nurse.classList.add("image-transform");
+    homeCard.classList.add("card-transform");
+    visitorPage.classList.add("visitor-page-show");
+    showVisitor.style.display = "block";
+  }
+});
